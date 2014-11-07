703d172edfe367942e8fa72e386bd346839a6cae
rafael.dougllas@gmail.com <rafael.dougllas@gmail.com@c0c4ab4d-4cb5-3ec5-444f-39854978a1a2>
2014-11-07 02:52:58 +0000

    Adicionei a busca binaria ao codigo de alocamento de buffer

--- trunk/Codes/AlgoritmoGuloso.py
+++ trunk/Codes/AlgoritmoGuloso.py
@@ -1,4 +1,4 @@
-
+import copy;
 listaMemorias    =  [['memoria1',6,6,80],['memoria2',7,7,80]]; #(nome,capacidade,qtdPortas,larguraBanda)
 
 listaBuffers    = [['bufferA',4,4,4],['bufferB',4,4,2],['bufferC',2,2,1],['bufferD',3,3,4]]; #(nome,capacidade,qtdPortas,taxaAcesso)
@@ -14,12 +14,12 @@ def ordenarListaTupla(lista,i):
     lista.sort(key=lambda x: x[i]);
     return lista;
 
-def alocaBuffer(lmemorias,lbuffers):
+def alocaBuffer(lmemorias,lbuffers,f):
         qtdBuffers  = len(lbuffers);
         qtdMemorias = len(lmemorias);
 
-        listaMemoriasAux = lmemorias;
-        listaBuffersAux   = lbuffers;
+        listaMemoriasAux = copy.deepcopy(lmemorias);
+        listaBuffersAux   = copy.deepcopy(lbuffers);
         
         ordenarListaTupla(listaBuffersAux,3);#ordena pela taxa de acesso
 
@@ -28,7 +28,8 @@ def alocaBuffer(lmemorias,lbuffers):
         for i, bufer in enumerate(listaBuffersAux):   
                 alocouBuffer = False;    
                 for j, memoria in enumerate(listaMemoriasAux):
-                        if(memoria[1]>=bufer[1] and memoria[2]>=bufer[2]):
+                        print("memoria%s cap:%i porta: %i banda: %i"%(memoria[0],memoria[1],memoria[2],memoria[3]))
+                        if(memoria[1]>=bufer[1] and memoria[2]>=bufer[2] and memoria[3]>=(bufer[3]*f)):
                                 varAuxCapNew = memoria[1] - bufer[1]; 
                                 memoria[1] = varAuxCapNew;
                                 varAuxPortNew = memoria[2] - bufer[2];
@@ -39,8 +40,36 @@ def alocaBuffer(lmemorias,lbuffers):
                 if(alocouBuffer==True):
                         alocouBuffer = False;                        
                 else:
+                        listaMemoriasAux = [];
+                        listaBuffersAux  = [];
                         return False;#nao alocou algum buffer
+        listaMemoriasAux = [];
+        listaBuffersAux  = [];
         return True;#alocou todos os buffers
 
 
 
+
+
+
+def maximizaF(linf,lsup):
+    f = (lsup+linf)/2
+
+    while (lsup - linf  > 1):
+       
+        result = alocaBuffer(listaMemorias,listaBuffers,f)
+        if result:
+            linf = f
+            f = (lsup+linf)/2
+            print("alocou com f %i"%(f));
+        else:
+            lsup = f
+            f = (lsup+linf)/2
+            print("nao alocou com f %i"%(f));
+linf = 1
+lsup = 200
+maximizaF(linf,lsup);
+
+
+
+    
